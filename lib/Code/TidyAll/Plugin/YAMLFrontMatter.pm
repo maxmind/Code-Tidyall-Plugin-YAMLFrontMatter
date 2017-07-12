134c0ca91b755a029e06d2003df08d3d3a5c74e6
Mark Fowler <mark@twoshortplanks.com>
2017-07-12 11:37:37 -0400

    Initial dzil build
    - Autogen files
    - Code tidy
    - Documentation changes

--- lib/Code/TidyAll/Plugin/YAMLFrontMatter.pm
+++ lib/Code/TidyAll/Plugin/YAMLFrontMatter.pm
@@ -2,23 +2,34 @@ package Code::TidyAll::Plugin::YAMLFrontMatter;
 
 use strict;
 use warnings;
+use namespace::autoclean;
 
 our $VERSION = '1.000000';
 
 use Moo;
 
-use Encode;
+use Code::TidyAll::Util qw( read_file );
+use Encode qw( decode encode FB_CROAK );
 use Try::Tiny qw( catch try );
 use YAML::XS qw( Load );
 
 extends 'Code::TidyAll::Plugin';
 
-# This regular expression is taken directly from the Jekyll source code here:
+# This regular expression is based on the regex
+#     \A(---\s*\n.*?\n?)^((---|\.\.\.)\s*$\n?)    (with the m flag)
+# from the Jekyll source code here:
 # https://github.com/jekyll/jekyll/blob/c7d98cae2652b2df7ebd3c60b4f8c87950760e47/lib/jekyll/document.rb#L13
-my $YAML_REGEX = qr!\A(---\s*\n.*?\n?)^((---|\.\.\.)\s*$\n?)!m;
+my $YAML_REGEX = qr{
+   \A
+      # the starting ---, and anything up until...
+      (---\s*\n.*?\n?)
+
+      # ...the first --- or ... on their own line
+      ^ (?:---|\.\.\.) \s* $ \n?
+}mx;
 
 has encoding => (
-    is  => 'ro',
+    is => 'ro',
 
     # By default Jekyll 2.0 and later defaults to utf-8, so this seems
     # like a sensible default for us
@@ -27,13 +38,15 @@ has encoding => (
 
 has required_top_level_keys => (
     is      => 'ro',
-    default => '',
+    default => q{},
 );
 
 has _req_keys_hash => ( is => 'lazy' );
+
 sub _build__req_keys_hash {
     my $self = shift;
     return +{
+
         # note use of magical split on space to do automatic trimming
         map { $_ => 1 } split q{ }, $self->required_top_level_keys
     };
@@ -42,45 +55,41 @@ sub _build__req_keys_hash {
 sub validate_file {
     my ( $self, $filename ) = @_;
 
-    # read in the file
-    my $src = do {
-        open my $fh, '<:bytes', $filename
-            or die "Cannot open file '$filename': $!";
-        local $/ = undef;
-        <$fh>;
-    };
+    my $src = read_file( $filename, binmode => ':bytes' );
 
     # YAML::XS always expects things to be in UTF-8 bytes
     my $encoding = $self->encoding;
     try {
-        $src = decode( $encoding, $src, Encode::FB_CROAK);
-        $src = encode( 'UTF-8', $src, Encode::FB_CROAK );
-    } catch {
+        $src = decode( $encoding, $src, FB_CROAK );
+        $src = encode( 'UTF-8', $src, FB_CROAK );
+    }
+    catch {
         die "File does not match encoding '$encoding': $_";
     };
 
     # is there a BOM?  There's not meant to be a BOM!
-    if ($src =~ /\A\x{EF}\x{BB}\x{BF}/) {
+    if ( $src =~ /\A\x{EF}\x{BB}\x{BF}/ ) {
         die "Starting document with UTF-8 BOM is not allowed\n";
     }
 
     # match the YAML front matter.
-    unless ($src =~ $YAML_REGEX) {
+    my $yaml;
+    unless ( ($yaml) = $src =~ $YAML_REGEX ) {
         die "'$filename' does not start with valid YAML Front Matter\n";
     }
-    my $yaml = $1;
 
     # parse the YAML front matter.
-    my $ds =  try {
-        Load( $yaml );
-    } catch {
+    my $ds = try {
+        Load($yaml);
+    }
+    catch {
         die "Problem parsing YAML: $_";
     };
 
     # check for required keys
-    my $errors = '';
-    for (sort keys %{ $self->_req_keys_hash }) {
-        next if $ds->{ $_ };
+    my $errors = q{};
+    for ( sort keys %{ $self->_req_keys_hash } ) {
+        next if $ds->{$_};
         $errors .= "Missing required YAML Front Matter key: '$_'\n";
     }
     die $errors if $errors;
@@ -98,7 +107,7 @@ In your .tidyallrc file:
 
     [YAMLFrontMatter]
     select = **/*.md
-    required_top_level_key = title layout
+    required_top_level_keys = title layout
 
 =head1 DESCRIPTION
 
@@ -119,13 +128,13 @@ It will complain if:
 
 =item The YAML Front Matter is missing one or more configured top level keys
 
-=cut
+=back
 
 =head2 Options
 
 =over
 
-=item C<required_top_level_key>
+=item C<required_top_level_keys>
 
 Keys that must be present at the top level of the YAML Front Matter.
 
@@ -136,6 +145,11 @@ later.)
 
 =back
 
+=head1 SUPPORT
+
+Please report all issues with this code using the GitHub issue tracker at
+L<https://github.com/maxmind/Code-TidyAll-Plugin-YAMLFrontMatter/issues>.
+
 =head1 SEE ALSO
 
-L<Jeykll's Front Matter Documentation|https://jekyllrb.com/docs/frontmatter/>
+L<Jekyll's Front Matter Documentation|https://jekyllrb.com/docs/frontmatter/>
--- t/lib/Test/Code/TidyAll/Plugin/YAMLFrontMatter.pm
+++ t/lib/Test/Code/TidyAll/Plugin/YAMLFrontMatter.pm
@@ -1,5 +1,8 @@
 package Test::Code::TidyAll::Plugin::YAMLFrontMatter;
 
+use strict;
+use warnings;
+
 use Test::Class::Most;
 
 use Code::TidyAll;
@@ -16,26 +19,29 @@ sub tidyall {
         root_dir => tempdir_simple(),
         plugins  => {
             YAMLFrontMatter => {
-                select => '*',  # everything, since we're testing
+                select => '*',    # everything, since we're testing
                 %{ $args{conf} || {} },
             },
         },
     );
 
     my $result;
-    my ( $output, $error ) = capture {
+    capture {
         $result = $tidyall->process_source( $args{source}, 'dummy.md' );
     };
 
     my $expect_error = $args{expect_error};
-    unless ($expect_error ) {
+    unless ($expect_error) {
         is( $result->state, 'checked', "state=checked [$args{desc}]" );
         is( $result->error, undef,     "no error [$args{desc}]" );
         return;
     }
 
     is( $result->state, 'error', "state=error [$args{desc}]" );
-    like( $result->error || '', $expect_error, "error message [$args{desc}]" );
+    like(
+        $result->error || q{}, $expect_error,
+        "error message [$args{desc}]"
+    );
     return;
 }
 
@@ -48,13 +54,14 @@ sub test_main : Tests {
     );
 
     $self->tidyall(
-        source       => 'fish',
-        expect_error => qr/dummy[.]md' does not start with valid YAML Front Matter/,
-        desc         => 'no front matter',
+        source => 'fish',
+        expect_error =>
+            qr/dummy[.]md' does not start with valid YAML Front Matter/,
+        desc => 'no front matter',
     );
 
     $self->tidyall(
-        source => qq{---\n!!!!!\n---\nThis should not work},
+        source       => qq{---\n!!!!!\n---\nThis should not work},
         expect_error => qr/Problem parsing YAML/,
         desc         => 'Bad YAML',
     );
@@ -64,19 +71,20 @@ sub test_main : Tests {
     $self->tidyall(
         source => "\x{EF}\x{BB}\x{BF}---\n---\nSomeone set us up the BOM",
         expect_error => qr/Starting document with UTF-8 BOM is not allowed/,
-        desc   => 'BOM',
+        desc         => 'BOM',
     );
 
     $self->tidyall(
-        source => "\x{fe}\x{ff}\x{00}-\x{00}-\x{00}-\x{00}\n\x{00}-\x{00}-\x{00}-\x{00}\n\x{00}!",
-        conf   => { encoding => 'UTF-16' },
-        desc   => 'UTF-16',
+        source =>
+            "\x{fe}\x{ff}\x{00}-\x{00}-\x{00}-\x{00}\n\x{00}-\x{00}-\x{00}-\x{00}\n\x{00}!",
+        conf => { encoding => 'UTF-16' },
+        desc => 'UTF-16',
     );
 
     $self->tidyall(
-        source => "---\n---\nL\x{e9}on hates invalid UTF-8",
+        source       => "---\n---\nL\x{e9}on hates invalid UTF-8",
         expect_error => qr/File does not match encoding 'UTF-8'/,
-        desc   => 'Invalid encoding',
+        desc         => 'Invalid encoding',
     );
 
     ## required keys
@@ -89,9 +97,9 @@ sub test_main : Tests {
     );
 
     $self->tidyall(
-        source       => "---\ntitle: B Piper\n---\nA rose by any other name",
-        desc         => 'No missing key',
-        conf         => { required_top_level_keys => 'title' },
+        source => "---\ntitle: B Piper\n---\nA rose by any other name",
+        desc   => 'No missing key',
+        conf   => { required_top_level_keys => 'title' },
     );
 
     $self->tidyall(
